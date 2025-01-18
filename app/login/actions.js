551fffb073418a6b05d99a87a67ad5d6eb297663
Himanshu <himanshunani9@gmail.com>
2025-01-18 21:45:42 +0530

    added dynamic redirect URLs based on env (test 3)

--- app/login/actions.js
+++ app/login/actions.js
@@ -4,6 +4,18 @@ import { redirect } from "next/navigation";
 import { createClient } from "../utils/supabase/server";
 import { revalidatePath } from "next/cache";
 
+const getURL = () => {
+    let url =
+        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+        'http://localhost:3000/'
+    // Make sure to include `https://` when not localhost.
+    url = url.startsWith('http') ? url : `https://${url}`
+    // Make sure to include a trailing `/`.
+    url = url.endsWith('/') ? url : `${url}/`
+    return url
+}
+
 export async function login(formData) {
     const supabase = await createClient();
     const data = {
@@ -23,20 +35,17 @@ export async function login(formData) {
 }
 
 export async function signup(formData) {
-    const supabase = createClient();
+    const supabase = await createClient();
     const data = {
         email: formData.get('email'),
         password: formData.get('password')
     }
 
-    const redirectTo =
-        process.env.NODE_ENV === 'production'
-            ? 'https://dad-jokes-virid-beta.vercel.app/'
-            : 'http://localhost:3000';
-
-    const {error} = (await supabase).auth.signUp({
+    const {error} = await supabase.auth.signUp({
         ...data,
-        options: { redirectTo }
+        options: {
+            redirectTo: getURL(),
+          },
     });
 
     if (error) {
