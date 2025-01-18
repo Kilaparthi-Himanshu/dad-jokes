1aab6c3298d2ffb125fc4d8df5256d5d2e1921df
Himanshu <himanshunani9@gmail.com>
2025-01-19 00:12:22 +0530

    removed getURL (TEST-9)

--- app/login/actions.js
+++ app/login/actions.js
@@ -4,19 +4,6 @@ import { redirect } from "next/navigation";
 import { createClient } from "../utils/supabase/server";
 import { revalidatePath } from "next/cache";
 
-const getURL = () => {
-    let url =
-        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
-        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
-        'http://localhost:3000/'
-
-    // Make sure to include `https://` when not localhost.
-    url = url.startsWith('http') ? url : `https://${url}`
-    // Make sure to include a trailing `/`.
-    url = url.endsWith('/') ? url : `${url}/`
-    return url
-}
-
 export async function login(formData) {
     const supabase = await createClient();
     const data = {
@@ -42,12 +29,7 @@ export async function signup(formData) {
         password: formData.get('password')
     }
 
-    const {error} = await supabase.auth.signUp({
-        ...data,
-        options: {
-            emailRedirectTo: getURL(),
-          },
-    });
+    const {error} = await supabase.auth.signUp({data});
 
     if (error) {
         console.error(error);
